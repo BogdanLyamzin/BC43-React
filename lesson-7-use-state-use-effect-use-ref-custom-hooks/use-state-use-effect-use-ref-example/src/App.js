9aaca89a14b5588411b333813894b64943b38c8b
unknown <bogdan.lyamzin.d@gmail.com>
2023-02-23 10:34:50 +0200

    add use-state example

---
+++ lesson-7-use-state-use-effect-use-ref-custom-hooks/use-state-use-effect-use-ref-example/src/App.js
@@ -0,0 +1,27 @@
+import Navbar from "./modules/Navbar/Navbar";
+import ToggleButton from "./shared/components/ToggleButton/ToggleButton";
+import Vote from "./modules/Vote/Vote";
+import MyBooks from "./modules/MyBooks/MyBooks";
+import Posts from "./modules/Posts/Posts";
+import PostsSearch from "./modules/PostsSearch/PostsSearch";
+
+import navbarMenuItems from "./data/navbarMenuItems.json";
+
+import "./shared/styles/styles.scss";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar menuItems={navbarMenuItems} /> */}
+      <div className="container">
+        {/* <PostsSearch /> */}
+        {/* <Posts /> */}
+        {/* <ToggleButton text="Click me" /> */}
+        {/* <Vote /> */}
+        <MyBooks />
+      </div>
+    </>
+  );
+}
+
+export default App;
